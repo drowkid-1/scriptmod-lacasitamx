d046dff882ba1adcb75fbcfc25ca4c1b78b9194f
⠀⠀⠀⠀ <pxne031112hmcrgda1@bggem.mx>
2023-12-22 22:44:51 -0600

    update

--- ext/xd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCPdir="."
-
-   wget -O chekuser.sh https://www.dropbox.com/s/dypjomdhjhkpahk/chekuser.sh &>/dev/null
-   wget -O chekuser.py https://www.dropbox.com/s/7he83qd5pfznrvh/chekuser.py &>/dev/null
